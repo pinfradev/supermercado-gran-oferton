5a06ea73bdb7e2089e28a0cdb39ccc77bce0ebbf
Fray <pinfradev@gmail.com>
2023-04-18 11:30:32 -0400

    changing color to gray

--- src/Views/Home/Components/Footer.jsx
+++ src/Views/Home/Components/Footer.jsx
@@ -1,7 +1,7 @@
 import './Footer.css';
 const Footer = () => {
     return (
-        <footer className="footer__market bg-black p-4">
+        <footer className="footer__market bg-black2 p-4">
           <div className="container p-0 d-flex justify-content-between align-items-center">
             <span className='footer__legal'>® 2023 Poli Inc todos los derechos reservados</span>
             <ul className="content__list d-flex content__list--gap m-0 align-items-center">
