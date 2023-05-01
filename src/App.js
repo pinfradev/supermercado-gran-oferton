3f2b2dc22c93c80d9b5f3dd2cef4ad9b01759dd9
Fray <pinfradev@gmail.com>
2023-04-30 23:04:03 -0400

    admin login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { Home } from './Views/Home/Home';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { ProductsList } from './Views/ProductsList/ProductsList';
 import { ProductDetail } from './Views/ProductDetail/ProductDetail';
+import { AdminLogin } from './Views/AdminLogin/AdminLogin';
 import './App.css';
+import { AdminPage } from './Views/AdminPage/AdminPage';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
             <Route path='/' element={<Home/>}/>
             <Route path='/products-list' element={<ProductsList/>}/>
             <Route path='/product-detail' element={<ProductDetail/>}/>
+            <Route path='/admin' element={<AdminLogin/>}/>
+            <Route path="/administrator" element={<AdminPage/>}/>
             <Route path='*' element={<p>Not found 404</p>}/>
         </Routes>
     </HashRouter>
